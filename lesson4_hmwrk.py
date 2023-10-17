bb75f7cb503599c8e6cac0b03bf08e1451b3bae7
vldslvmvchn <vladmov05@gmail.com>
2023-10-17 22:52:21 +0300

    lesson4domawka

---
+++ lesson4_hmwrk.py
@@ -0,0 +1,31 @@
+# 1
+
+# new_value = int
+# new_value = int(input("Input a number 1: "))
+# print(new_value / 2) if new_value < 100 else print(new_value * -1)
+
+
+# 2
+
+# new_value = int
+# new_value = int(input("Input a number 1: "))
+# print("1") if new_value < 100 else print("0")
+
+
+# 3
+
+# new_value = int
+# new_value = int(input("Input a number 1: "))
+# print(True) if new_value < 100 else print(False)
+
+# 4
+
+# new_str = str
+# new_str = input("Type a word : ")
+# print(new_str * 2) if len(new_str) < 5 else print(new_str)
+
+# 5
+
+# new_str = str
+# new_str = input("Type a word :")
+# print(new_str, new_str[::-1]) if len(new_str) < 5 else print(new_str)

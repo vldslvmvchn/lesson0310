34b74a53736bd9363f57d3b4a3913d51344b819e
vldslvmvchn <vladmov05@gmail.com>
2023-10-22 19:36:53 +0300

    homework5

---
+++ hmwrk5.py
@@ -0,0 +1,62 @@
+############### 1) ###############
+
+# value_str = "Hello my name is Vlad, i'm a student of it-school Hillel!"
+
+#a)
+# index = 3
+# print(value_str[index])
+
+#b)
+# print(value_str[-2:-1])
+
+#c)
+# print(value_str[:5])
+
+#d)
+# print(value_str[:-2])
+
+#e)
+# print(value_str[::2])
+
+#f)
+# print(value_str[1::2])
+
+#g)
+# print(value_str[::-1])
+
+#h)
+# print(value_str[::-2])
+
+#i)
+# print(len(value_str))
+
+
+############# 2) ##############
+
+# value_str = "Hello my name is Vlad, i'm a student of it-school Hillel!"
+# print(value_str[:-1].count(" ") + 1)
+
+
+############## 3) ###############
+
+# s = input("Type a string ")
+# ch = input("Type a letter ")
+# index = -1
+# while True:
+#     index = s.find(ch, index + 1)
+#     if index == -1:
+#         break
+#     print(index)
+
+
+
+
+
+############## 4) ################
+
+# value_str = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+# string_up = value_str.upper()
+# new = list(string_up)
+# new[0] = 'h'
+# new[-1] = 'h'
+# print(''.join(new))
